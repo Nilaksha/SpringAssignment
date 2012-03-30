237c9f88d9e93d49f40f885146a1893b431578dd
Nilaksha <nilakshaperera@gmail.com>
2012-03-30 20:33:53 +0530

    Update src/org/cse/springtute/SimpleStudentRepository.java

--- src/org/cse/springtute/SimpleStudentRepository.java
+++ src/org/cse/springtute/SimpleStudentRepository.java
@@ -1,41 +1,39 @@
 package org.cse.springtute;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class SimpleStudentRepository implements StudentRepository {
 
-	private Map<Long, Student> studentsDb = new HashMap<Long, Student>();
+    private Map<Long, Student> studentsDb = new HashMap<Long, Student>();
 
-	@Override
-	public void saveStudent(Student stu) {
-		// TODO Auto-generated method stub
+    @Override
+    public void saveStudent(Student stu) {
+        studentsDb.put(stu.getRegNumber(), stu);
+    }
 
-	}
+    @Override
+    public void deleteStudent(Student stu) {
+        studentsDb.remove(stu.getRegNumber());
+    }
 
-	@Override
-	public void deleteStudent(Student stu) {
-		// TODO Auto-generated method stub
+    @Override
+    public Student findStudent(long regNumber) {
+        return studentsDb.get(regNumber);
+    }
 
-	}
+    @Override
+    public void updateStudent(Student stu) {
+        studentsDb.remove(stu.getRegNumber());
+        studentsDb.put(stu.getRegNumber(), stu);
+    }
 
-	@Override
-	public Student findStudent(long regNumber) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void updateStudent(Student stu) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public List<Student> findAllStudents() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
+    @Override
+    public List<Student> findAllStudents() {
+        ArrayList<Student> studentsList = new ArrayList<Student>();
+        studentsList.addAll(studentsDb.values());
+        return studentsList;
+    }
 }
