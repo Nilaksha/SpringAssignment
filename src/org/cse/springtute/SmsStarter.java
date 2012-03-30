9527f91568a1da4c2382fb80f3af117ab6a1753a
Nilaksha <nilakshaperera@gmail.com>
2012-03-30 20:34:43 +0530

    Update src/org/cse/springtute/SmsStarter.java

--- src/org/cse/springtute/SmsStarter.java
+++ src/org/cse/springtute/SmsStarter.java
@@ -1,12 +1,30 @@
 package org.cse.springtute;
 
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class SmsStarter {
 
-	public static void main(String[] args) {
-		StudentManagementSystem sms;
-		//load the bean from spring
-//		sms.listAllStudents();
-//		sms.registerStudent(stu);
-//		sms.listAllStudents();
-	}
+    public static void main(String[] args) {
+
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring-context.xml");
+        context.start();
+
+        Student std = (Student) context.getBean("student1");
+
+        StudentManagementSystem sms = (StudentManagementSystem) context.getBean("sms");
+
+        sms.listAllStudents();
+
+        System.out.println("\n#######################################\n");
+        
+        std = (Student) context.getBean("student4");
+        sms.registerStudent(std);
+        std = (Student) context.getBean("student5");
+        sms.registerStudent(std);
+        std = (Student) context.getBean("student6");
+        sms.registerStudent(std);
+        
+        sms.listAllStudents();
+    }
 }
