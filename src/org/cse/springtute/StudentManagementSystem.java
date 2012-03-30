92693713c3aea117fdf7d0c75d03816a5bf152ed
Nilaksha <nilakshaperera@gmail.com>
2012-03-30 20:38:30 +0530

    Update src/org/cse/springtute/StudentManagementSystem.java

--- src/org/cse/springtute/StudentManagementSystem.java
+++ src/org/cse/springtute/StudentManagementSystem.java
@@ -1,14 +1,24 @@
 package org.cse.springtute;
 
+import java.util.ArrayList;
+
 public class StudentManagementSystem {
 
-	private StudentRepository repo;
+    private StudentRepository repo;
 
-	public void listAllStudents() {
-		//Get all the students from repository and print them in the screen
-	}
+    public StudentManagementSystem(StudentRepository repo) {
+        this.repo = repo;
+    }
 
-	public void registerStudent(Student stu){
-		//Save the new student using repository
-	}
+    public void listAllStudents() {
+        ArrayList<Student> students = new ArrayList<Student>();
+        students.addAll(repo.findAllStudents());
+        for(Student stu : students){
+            System.out.println("Name: "+stu.getFirstName()+ " " + stu.getLastName() + "\tRegNo: "+ stu.getRegNumber());
+        }
+    }
+
+    public void registerStudent(Student stu) {
+        repo.saveStudent(stu);
+    }
 }
